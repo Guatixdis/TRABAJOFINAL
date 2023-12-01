2534c6aae1e4f4aed7372d2c96f79079f1558f1b
Proplayer143789 <alejandroson28@gmail.com>
2023-11-30 23:24:40 -0500

    Cambio final SON

--- PROYECTO/src/pages/busquedaAdmin/[id].js
+++ PROYECTO/src/pages/busquedaAdmin/[id].js
@@ -10,7 +10,7 @@ const BusquedaUsuarioId = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:3080/recurso/listar');
+        const response = await fetch('http://localhost:3080/api/recurso/listar');
         if (!response.ok) {
           throw new Error('Error al obtener datos del servidor');
         }
